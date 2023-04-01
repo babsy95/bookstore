af3e2fc76dd0eb272cef3fedcb69829258270891
babsy95 <babsy.b.95@gmail.com>
2023-04-01 19:05:42 -0400

    db

--- includes/mysqli_oop_connect.php
+++ includes/mysqli_oop_connect.php
@@ -1,10 +1,16 @@
 
 <?php
 
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'book-store');
+    // define('DB_USER', 'root');
+    // define('DB_PASSWORD', '');
+    // define('DB_HOST', 'localhost');
+    // define('DB_NAME', 'book-store');
+
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'babsy1234');
+    define('DB_HOST', 'bookstore.cyucnklcjixy.us-east-2.rds.amazonaws.com');
+    define('DB_PORT', '3306');
+    define('DB_NAME', 'bookstore');
 
     $mysqli = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
